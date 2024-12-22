bb39dfe07da3632801de99f51a7643f407f709b2
Dev-AmanGoswami <aman.goswami.dev@gmail.com>
2024-12-22 11:10:35 +0530

    Implemented: Generating OTP and sending via Gmail.

--- services/otp-service.js
+++ services/otp-service.js
@@ -1,23 +1,37 @@
 const crypto = require('crypto');
 const hashService = require('./hash-service');
-const smsSid = process.env.SMS_SID;
-const smsAuthToken = process.env.SMS_AUTH_TOKEN;
-const twilio = requrie('twilio')(smsSid,smsAuthToken,{
-    lazyLoading: true,
-})
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.SENDER_EMAIL,
+        pass: process.env.SENDER_PASSWORD
+    }
+});
+
 
 class OtpService{
     async generateOtp() {
         const otp = crypto.randomInt(1000,9999);
         return otp;
     }
-    async sendBySms() {
-        return twilio.messages.create({
-            to: phoneNumber,
-            from: process.env.SMS_FROM_NUMBER,
-            body: `Your codershouse OTP is ${otp}`
-        }) 
-    }   
+    async sendByEmail(email,otp) {
+        console.log("Aman: ",process.env.SENDER_EMAIL,process.env.SENDER_PASSWORD);
+        const mailOptions = {
+            from: process.env.SENDER_EMAIL,
+            to: email,
+            subject: 'EchoRoom Authentication',
+            text: `Dear User,\n\nYour One-Time Password (OTP) for EchoRoom authentication is:\n\n${otp}\n\nThis OTP is valid for the next 10 minutes. Please do not share this code with anyone.\n\nIf you did not request this OTP, please contact our support team immediately at echoroom.support@gmail.com.\n\nThank you for choosing EchoRoom!\n\nBest regards,\nThe EchoRoom Team`
+        }
+        return await transporter.sendMail(mailOptions, function(error, info){
+            if (error) {
+              console.log(error);
+            } else {
+              console.log('Email sent: ' + info.response);
+            }
+        });
+    }
     verifyOtp(hashedOtp, data) {
         let computedHash = hashService.hashOtp(data);
         return computedHash === hashedOtp;
